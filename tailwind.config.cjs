144ba9bfd4382e2135f256afd1dae2aa4b9334df
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-09-02 14:58:34 +0200

    video

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,10 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			sans: ["Gotham SSm A, sans-serif"]
+		}
 	},
+	extend: {},
 	plugins: [],
 }

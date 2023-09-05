635a3f65431808544603412a3887217513f39b93
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-09-05 19:03:30 +0200

    prueba enrutamiento contacto

---
+++ src/pages/about.md
@@ -0,0 +1,7 @@
+---
+Title: "About"
+---
+
+<h1>
+Acerca de mi
+</h1>
---
+++ src/routes/astro.config.mjs
@@ -0,0 +1,7 @@
+export default {
+    // Otras configuraciones
+    pages: {
+      '/contacto': 'src/routes/contacto.astro',
+      // Agrega las rutas para tus componentes aquí
+    },
+  };
